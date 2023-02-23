dd7c9809cacefa8bc7ffd1a0384a46f10769b713
Marianne <105245058+smariannee@users.noreply.github.com>
2023-02-22 22:08:39 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const { app } = require("./config/express");
 
 const hostname = '44.213.233.39';
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const main = () => {
+    try {
+        app.listen(app.get('port'), hostname);
+        console.log(`Server is running in http://${hostname}:${app.get('port')}/`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
